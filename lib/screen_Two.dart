7dc665c8eaf00a9934a7aae97aa1023a578bec05
abdul razzaq <ar8937562@gmail.com>
2023-08-07 11:13:57 -0700

    created screen two to manage routing between screen one and screen two

---
+++ lib/screen_Two.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_getx/home_screen.dart';
+import 'package:get/get.dart';
+
+class ScreenTwo extends StatefulWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenTwo> createState() => _ScreenTwoState();
+}
+
+class _ScreenTwoState extends State<ScreenTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Screen Two"),),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:  [
+          TextButton(onPressed: (){
+            // Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+            //  Get.back();
+            // Get.to(const HomeScreen());
+            Get.back();
+            Get.back();
+          }, child: const Center(child: Text('GO TO HOME SCREEN')))
+        ],
+      ),
+    );
+  }
+}
