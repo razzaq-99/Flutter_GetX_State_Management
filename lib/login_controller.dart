04037d11b5b295e1a73b1108a09514edbc8df911
abdul razzaq <ar8937562@gmail.com>
2023-08-12 10:38:14 -0700

    created login controller to manage and control the getx and obs and created post api method

---
+++ lib/login_controller.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart';
+
+class LoginController extends GetxController {
+  final emailController = TextEditingController().obs;
+  final passwordCOntroller = TextEditingController().obs;
+
+  RxBool loading = false.obs;
+  void loginApi() async {
+    loading.value = true;
+    try {
+      final response =
+          await post(Uri.parse('https://reqres.in/api/login'), body: {
+        'email': emailController.value.text,
+        'password': passwordCOntroller.value.text,
+      });
+
+      var data = jsonDecode(response.body);
+      print(response.statusCode);
+      print(data);
+      if (response.statusCode == 200) {
+        loading.value = false;
+        Get.snackbar('Login Successful','Congrats');
+      } else {
+        loading.value = false;
+        Get.snackbar('Login Failed', data['error']);
+      }
+    } catch (e) {
+      loading.value = false;
+      Get.snackbar('Exception', e.toString());
+    }
+  }
+}
