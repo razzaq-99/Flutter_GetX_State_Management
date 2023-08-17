b4041159b90553599b7fbbd53a0bfacf5c03cd49
abdul razzaq <ar8937562@gmail.com>
2023-08-17 09:38:18 -0700

    added email and password hints to manage these through any languages in the future

--- lib/getx_mvvm/resources/getx_localization/languages.dart
+++ lib/getx_mvvm/resources/getx_localization/languages.dart
@@ -1,22 +1,20 @@
-
-
 import 'package:get/get_navigation/get_navigation.dart';
 
-class Languages extends Translations{
+class Languages extends Translations {
   @override
   // TODO: implement keys
   Map<String, Map<String, String>> get keys => {
-    'en_US' : {
-      'email_hint' : 'Enter Email',
-      'internet_exception' : 'We are unable to show results \nplease check your internet connection',
-      'general_exception' : 'still some issues in your coonection\ntry again please',
-      'welcome_back' : 'Welcome Back Again!',
-    },
-    'ur_PK' : {
-      'email_hint' : 'ای میل درج کریں۔'
-    },
-
-  };
-
-
+        'en_US': {
+          // 'email_hint': 'Enter Email',
+          'internet_exception':
+              'We are unable to show results \nplease check your internet connection',
+          'general_exception':
+              'still some issues in your coonection\ntry again please',
+          'welcome_back': 'Welcome Back Again!',
+          'login': 'Login please',
+          'email_hint': 'Email',
+          'password_hint': 'Password',
+        },
+        'ur_PK': {'email_hint': 'ای میل درج کریں۔'},
+      };
 }
