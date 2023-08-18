a01dba74428d7006e05d94064dfea405cbc0f840
abdul razzaq <ar8937562@gmail.com>
2023-08-18 10:56:26 -0700

    updated file to manage email controller and password controller through loginApi method

--- lib/getx_mvvm/view_model/controller/login_view_controller.dart
+++ lib/getx_mvvm/view_model/controller/login_view_controller.dart
@@ -1,13 +1,30 @@
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_getx/getx_mvvm/data/repository/login_repo/login_repository.dart';
+import 'package:flutter_getx/getx_mvvm/utils/utils.dart';
 import 'package:get/get.dart';
 
-class LoginViewController extends GetxController{
+class LoginViewController extends GetxController {
+  final _api = LoginRepo();
 
- final emailController = TextEditingController().obs;
- final passwordController = TextEditingController().obs;
+  final emailController = TextEditingController().obs;
+  final passwordController = TextEditingController().obs;
 
- final emailFocusNode = FocusNode().obs;
- final passwordFocusNode = FocusNode().obs;
+  final emailFocusNode = FocusNode().obs;
+  final passwordFocusNode = FocusNode().obs;
+
+  RxBool loading = false.obs;
+  void loginApi() {
+   loading.value = true;
+    Map data = {
+      'email': emailController.value.text,
+      'password': passwordController.value.text,
+    };
+    _api.loginApi(data).then((value) {
+     loading.value = false;
+     Utils.SnackBar('Login', 'Login Successfully');
+    }).onError((error, stackTrace) {
+     loading.value = false;
+      Utils.SnackBar('Error', error.toString());
+    });
+  }
 }
