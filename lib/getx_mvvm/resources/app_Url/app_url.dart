6b45d23e0d4300382ceb156f0023c8272405b952
abdul razzaq <ar8937562@gmail.com>
2023-08-20 05:23:43 -0700

    updated file because I have added userListApi url from get api

--- lib/getx_mvvm/resources/app_Url/app_url.dart
+++ lib/getx_mvvm/resources/app_Url/app_url.dart
@@ -6,4 +6,6 @@ class AppUrl {
   static const String baseUrl = 'https://reqres.in';
 
   static const String loginApi = '$baseUrl/api/login';
+
+  static const String userlistApi = 'https://webhook.site/f72baf1b-d5fb-475c-aa4e-866faa2fdb78';
 }
